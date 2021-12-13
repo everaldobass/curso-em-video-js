e39c18d9ffc5f0b85addc451373e9579cd22549b
Everaldo Nascimento <everaldobass@gmail.com>
2021-12-13 10:29:02 -0300

    modulo - E

---
+++ modulo-E/js/doWhile.js
@@ -0,0 +1,9 @@
+// Do While
+
+let cont = 1
+
+do{
+    console.log(`Valor do ${cont}`)
+    cont++
+
+}while(cont <= 10)
---
+++ modulo-E/js/moduloE.js
@@ -0,0 +1,8 @@
+// Repetição While
+
+let pizza = 1
+
+while(pizza <= 5){
+    console.log(`Comeu pizza ${pizza}`)
+    pizza++
+}
